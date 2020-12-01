1abfe578e0b055e665239881d119171c7125a267
Dhruv <spark.founder@gmail.com>
2020-12-01 20:36:22 +0530

    edit user file 1

--- src/User/User.js
+++ src/User/User.js
@@ -1,9 +1,11 @@
-import React from "react";
-
+import React ,{useState}from "react";
+import Calendar from 'react-calendar';
 import "./User.css";
 import Navbar from "../Navbar/Navbar";
+import 'react-calendar/dist/Calendar.css';
 
 function User() {
+    const [value, onChange] = useState(new Date());
     var today = new Date().toLocaleDateString(undefined, {
         day: '2-digit',
         month: '2-digit',
@@ -16,11 +18,17 @@ function User() {
   return (
     <div>
       <Navbar />
+      <div class="alert alert-info" role="alert">
+            Today is Holiday
+       </div>
       <div>
         <br />
         <h1 className='text-capitalize text-center text-info Time'>30 Days Left</h1>
         <h3 className='text-center text-muted'>JOINED ON</h3>
         <div className='Time_left text-capitalize text-center'>{today}</div>
+        <div>
+            <Calendar onChange={onChange} value={value} className='Calender'/>
+        </div>
       </div>
     </div>
   );
--- src/login/Login.js
+++ src/login/Login.js
@@ -17,19 +17,19 @@ function login() {
                     <h3 className="text-center">LOGIN</h3>
                     <form>
                         <div className="form-group">
-                            <label for="exampleInputEmail1">Email address</label>
+                            <label for="exampleInputEmail1 label">Email address</label>
                             <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
                             <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
                         </div>
                         <div class="form-group">
-                            <label for="exampleInputPassword1">Password</label>
+                            <label for="exampleInputPassword1 label">Password</label>
                             <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
                         </div>
                         <div class="form-che    ck">
                             <input type="checkbox" className="form-check-input" id="exampleCheck1" />
                             <label className="form-check-label" for="exampleCheck1">Check me out</label>
                         </div>
-                        <button type="submit" className="btn btn-primary">Submit</button>
+                        <button type="submit" className="btn btn-primary button_login ">Submit</button>
                     </form>
                 </div>
             </div>
